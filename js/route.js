d567d4bbe1e31f92eaef03bb1c0d47b572ddd0dd
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-05-02 14:22:06 +0200

    Added throttle and Debounce functions to prevent multi submit

--- js/route.js
+++ js/route.js
@@ -1,11 +1,11 @@
 //Route
 var Route = (function($, Controller, Foundation) {
   var bindAction =  function () {
-    Foundation.utils.S("#aggiungiMateria").on("click", function() {
+    Foundation.utils.S("#aggiungiMateria").on("click", Foundation.utils.throttle(function() {
       Foundation.utils.S("#addMateria").foundation('reveal', 'open');
-    });
+    }, 300));
 
-    Foundation.utils.S("#addMateriaInvia").on("click", Controller.saveMateria);
+    Foundation.utils.S("#addMateriaInvia").on("click", Foundation.utils.debounce(Controller.saveMateria, 300, true));
     //Foundation.utils.S("#statistiche").on("click", showStatistiche);
     //Foundation.utils.S("#aboutMe").on("click", showAboutMe);
   };
