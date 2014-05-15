bd89bb7e378ae6554ff88e3a999345d605ec8d0d
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-05-16 01:14:44 +0200

    Changed Directory

---
+++ web/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Here start the app
+ */
+
+//Foundation
+$(document).foundation({
+  accordion: {
+    multi_expand: false,
+    toggleable: true
+  }
+});
+//Route->Init function
+Route.init();
+
